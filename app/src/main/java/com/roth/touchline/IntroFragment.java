1681b0e27a76a9bd092dd8e19668927c5a9a22e1
iuraiura879 <iuraiura879@gmail.com>
2015-12-07 16:57:56 +0200

    images

--- app/src/main/java/com/roth/touchline/IntroFragment.java
+++ app/src/main/java/com/roth/touchline/IntroFragment.java
@@ -6,8 +6,10 @@ import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +18,7 @@ import android.view.animation.AlphaAnimation;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.Locale;
 
@@ -42,6 +45,12 @@ public class IntroFragment extends Fragment {
         final Button button = (Button) view.findViewById(R.id.button3);
         button.setPaintFlags(button.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
 
+        /*
+        TextView txt = (TextView) view.findViewById(R.id.Touchline);
+        //Typeface type = Typeface.createFromAsset(getActivity().getAssets(),"fonts/Kokila.ttf");
+         txt.setText(Html.fromHtml("<b>"+getString(R.string.touchline)+"<sup>+</sup></b>"));
+        //txt.setTypeface(type);
+*/
 
 
         return view;
